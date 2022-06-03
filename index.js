4745402203c64c254a52f145209db80c27547550
VanVolen <77025184+VanVolen@users.noreply.github.com>
2022-06-03 18:38:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("NIKOLAAAAAAAAAAAAAAA HEHEHEHEHEH NIKAD NECU GIT DA SKINEM!!!");
+  res.send("ajde ne");
 });
 
 app.listen(process.env.PORT || 5000);
