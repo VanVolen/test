7fdae6fa7372693850d4e4dc24c8fb0a718fb506
VanVolen <brano.sico@gmail.com>
2022-06-03 18:51:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const app = express();
 app.get("/", function (req, res) {
   res.send("POGODITE STA SAM SKINUO");
 });
+app.get('/',function(req,res) {
+  res.sendFile('index.html');
+});
 
 app.listen(process.env.PORT || 5000);
