0f8516e948191a37415f15d31300515858f5f130
VanVolen <brano.sico@gmail.com>
2022-06-03 20:53:56 +0200

    Sveta promena

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ const express = require("express");
 const app = express();
 const path = require('path');
 const router = express.Router();
+var $ = jQuery = require('jquery');
 
 // app.get("/", function (req, res) {
- //  res.send("POGODITE STA SAM SKINUO 12312412421");
+//  res.send("POGODITE STA SAM SKINUO 12312412421");
 // });
-app.get('/',function(req,res) {
-  res.sendFile(path.join(__dirname+'/index.html'));
-});
+//app.get('/',function(req,res) {
+//res.sendFile(path.join(__dirname+'/index.html'));
+//});
 
-// $(body).on(function(){
-//   $(this).addClass("svetaklasa");
-// })
+app.use(express.static('public'));
+
+var $ = jQuery = require('jquery');
+
+app.get('/',function(req,res) {
+res.sendFile(path.join(__dirname+'/test/jss/funckije.js'));
+});
 
 app.use('/', router);
 app.listen(process.env.PORT || 5000);
---
+++ public/funkcije.js
@@ -0,0 +1,4 @@
+console.log("axxxx");
+
+$("body").empty();
+
