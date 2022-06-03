f59619fd23ab696b8f64929a676f54e992235f0f
VanVolen <brano.sico@gmail.com>
2022-06-03 22:40:13 +0200

    1234

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require('path');
-const router = express.Router();
 var $ = jQuery = require('jquery');
+
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 // app.get("/", function (req, res) {
 //  res.send("POGODITE STA SAM SKINUO 12312412421");
@@ -11,13 +15,27 @@ var $ = jQuery = require('jquery');
 //res.sendFile(path.join(__dirname+'/index.html'));
 //});
 
-app.use(express.static('public'));
-
-var $ = jQuery = require('jquery');
-
-app.get('/',function(req,res) {
-res.sendFile(path.join(__dirname+'/test/jss/funckije.js'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-app.use('/', router);
-app.listen(process.env.PORT || 5000);
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(5000, () => {
+  console.log('listening on *:5000');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
