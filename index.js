940cea3d5ce0abdf103657f7da207870281d90d6
VanVolen <brano.sico@gmail.com>
2022-06-03 19:30:14 +0200

    Sveta poruka

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const app = express();
+const path = require('path');
+const router = express.Router();
 
-app.get("/", function (req, res) {
-  res.send("POGODITE STA SAM SKINUO");
-});
+// app.get("/", function (req, res) {
+ //  res.send("POGODITE STA SAM SKINUO 12312412421");
+// });
 app.get('/',function(req,res) {
-  res.sendFile('index.html');
+  res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-jQuery(body).on(function(){
-  jQuery(this).addClass("svetaklasa");
-})
+// $(body).on(function(){
+//   $(this).addClass("svetaklasa");
+// })
 
-
+app.use('/', router);
 app.listen(process.env.PORT || 5000);
