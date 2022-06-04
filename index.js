488c9479d43dfd5c9200872339248a13dc56a406
VanVolen <brano.sico@gmail.com>
2022-06-04 08:32:46 +0200

    Phaser Implementation

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ const io = new Server(server);
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/css.css');
+});
+
+app.use(express.static(__dirname + '/public'));
+
 
 io.on('connection', (socket) => {
   console.log('a user connected');
