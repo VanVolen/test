c3c85d7776e22d078785ee928bff5d76924e3094
VanVolen <brano.sico@gmail.com>
2022-06-05 22:26:47 +0200

    Update game.js

--- public/game.js
+++ public/game.js
@@ -4,6 +4,7 @@ var akey;
 var wkey;
 var dkey;
 var player;
+var skok = 0;
 window.onload = () => {
     /*ORBBBBBBBBBBBBBBBBB*/
     class Orb extends Phaser.GameObjects.Sprite {
@@ -15,6 +16,11 @@ window.onload = () => {
         }
     }
 
+    class Petar extends Phaser.Physics.Arcade.Sprite {
+        constructor(scene) {
+
+        }
+    }
     /*MAIN SCENA*/
     class MainScene extends Phaser.Scene {
         preload() {
@@ -23,32 +29,37 @@ window.onload = () => {
             this.load.image('ground', 'images/' + 'ground.png');
             this.load.image('orb', 'images/' + 'orb.png');
         }
+        // ISPALI METAK
         shootOrb(angle) {
             var orb = this.physics.add.image(player.x + 20, player.y - 50, "orb");
             this.physics.velocityFromRotation(angle, 1000, orb.body.velocity);
-            setTimeout(function(){
+            setTimeout(function () {
                 orb.destroy();
-                }, 1000);
+            }, 1000);
 
         }
         create() {
+            // DODAVANJE KEYBOARD MSM KEYS STAGOD
             spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
             wkey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
             akey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
             dkey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+            // -----------------------
             this.createPlayer();
             this.createPlatform();
             this.cursors = this.input.keyboard.createCursorKeys();
             this.physics.add.collider(player, this.platforms);
+            // ADD BUSH
             for (i = 0; i < 100; i++) {
                 var p = i - 50;
                 var value = Phaser.Math.Between(p * 100, (1 + p) * 100);
                 this.add.image(value, 605, "bush").setOrigin(1);
             }
+            // ON MOUSE CLICK ISPALI METAK
             var guza;
             guza = this;
             this.input.on('pointerup', function (pointer) {
-                var angle = Math.atan2(this.activePointer.worldY - player.y+60 , this.activePointer.worldX - player.x);
+                var angle = Math.atan2(this.activePointer.worldY - player.y + 60, this.activePointer.worldX - player.x);
                 console.log("Ugao: " + angle);
                 var angle = angle;
                 guza.shootOrb(angle);
@@ -71,15 +82,27 @@ window.onload = () => {
         }
         update() {
             this.cameras.main.startFollow(player, false, 0, 0.05, 0, 200);
+            // DAVANJE FUNKCIJE KEYEVIMA
             const moveAmt = 200;
             if (dkey.isDown) player.setVelocityX(moveAmt).flipX = false;
             if (akey.isDown) player.setVelocityX(-moveAmt).flipX = true;
-            if (spacebar.isDown && player.body.touching.down) { player.setVelocityY(-600); }
+
+            if (Phaser.Input.Keyboard.JustDown(spacebar) && (player.body.touching.down || skok == 1)) {
+                if (skok == 1) {
+                    player.setVelocityY(-500);
+                    skok = 0;
+                    console.log("skok 2");
+                    console.log(skok)
+                } else if (skok == 0) {
+                    skok = 1;
+                    player.setVelocityY(-500);
+                    console.log("skok 1")
+                }
+            }
+
             //this.physics.add.collider(player, platforms);
         }
-        penetrate() {
-            console.log("Svrsh");
-        }
+
     }
 
     const game = new Phaser.Game({
