096fc7f6c5310f4bc52a9029760c41299d916913
VanVolen <brano.sico@gmail.com>
2022-06-10 14:44:13 +0200

    fixing PORT

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 io.on('connection', (socket) => { console.log('a user connected'); });
-server.listen(3000, () => {/*console.log('listening on *:3000');*/ });
+server.listen(process.env.PORT ||3000 , () => {/*console.log('listening on *:3000');*/ });
 
 // - - - - - - - - - - STATIC FILES
 app.use(express.static("public"));
