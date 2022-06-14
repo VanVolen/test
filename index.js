a1d817288e624cf7ef484461acca379eb035d5b7
Nikola Petrovic <s.nino.petrovic@gmail.com>
2022-06-14 19:16:49 +0200

    websocket in progress

--- index.js
+++ index.js
@@ -1,63 +1,51 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
+
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-io.on('connection', (socket) => { console.log('a user connected'); });
-server.listen(process.env.PORT ||3000 , () => {/*console.log('listening on *:3000');*/ });
+var players = {};
+server.listen(process.env.PORT || 3000, () => {
+  /*console.log('listening on *:3000');*/
+});
 
 // - - - - - - - - - - STATIC FILES
 app.use(express.static("public"));
-app.use("/css", express.static(__dirname + "public/css"))
-app.use("/js", express.static(__dirname + "public/js"))
-app.use("/images", express.static(__dirname + "public/images"))
-app.use("/lib", express.static(__dirname + "public/lib"))
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "public/js"));
+app.use("/images", express.static(__dirname + "public/images"));
+app.use("/lib", express.static(__dirname + "public/lib"));
 
-app.get('/', (req, res) => { res.sendFile(__dirname + '/views/index.html'); });
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 // - - - - -  - -  CHAT
 
-// io.on('connection', (socket) => {
-//   socket.on('chat message', (msg) => {
-//     console.log('message: ' + msg);
-//   });
-// });
-
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
-  });
-});
-
-
-/*
 io.on("connection", (socket) => {
-  console.log("a user connected");
-});
-
-server.listen(process.env.PORT || 5000, () => {
-  console.log("listening on *:5000");
-});
-
-io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
-  });
-});
-io.emit("some event", {
-  someProperty: "some value",
-  otherProperty: "other value",
-}); // This will emit the event to all connected sockets
-
-io.on("connection", (socket) => {
+  console.log("connection created");
+  players[socket.id] = {
+    rotation: 0,
+    x: Math.floor(Math.random() * 700) + 50,
+    y: Math.floor(Math.random() * 500) + 50,
+    playerId: socket.id,
+    team: Math.floor(Math.random() * 2) == 0 ? "red" : "blue",
+  };
+  // send the players object to the new player
+  socket.emit("currentPlayers", players);
+  // update all other players of the new player
+  socket.broadcast.emit("newPlayer", players[socket.id]);
+  console.log(players);
   socket.on("chat message", (msg) => {
     io.emit("chat message", msg);
   });
-});*/
+  socket.on("disconnect", function () {
+    console.log("user disconnected");
+    // remove this player from our players object
+    delete players[socket.id];
+    // emit a message to all players to remove this player
+    io.emit("playerDisconnect", socket.id);
+  });
+});
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,19 +1,17 @@
 var socket = io();
 
 $(document).ready(function () {
-       $("#sendmessage").click(function (e) {
-              console.log("dali sam ja jquery god????")
-              e.preventDefault();
-              var msg = $(".chatinput").val();
-              if (msg) {
-                     console.log(msg);
-                     socket.emit('chat message', msg);
-                     $(".chatinput").val("");
-              }
-       });
-       socket.on('chat message', function (msg) {
-              $(".chatmessages").append("<li>" + msg + "</li>");
-              window.scrollTo(0, document.body.scrollHeight);
-       });
-
+  $("#sendmessage").click(function (e) {
+    e.preventDefault();
+    var msg = $(".chatinput").val();
+    if (msg) {
+      console.log(msg);
+      socket.emit("chat message", msg);
+      $(".chatinput").val("");
+    }
+  });
+  socket.on("chat message", function (msg) {
+    $(".chatmessages").append("<li>" + msg + "</li>");
+    window.scrollTo(0, document.body.scrollHeight);
+  });
 });
